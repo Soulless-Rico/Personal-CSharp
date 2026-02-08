e44aa54a2af5f8ba65f4dd7d1732ac8ff45c48b0
Soulless-Rico <soulless.rico.ai@gmail.com>
2026-02-08 09:31:55 +0100

    Auto-update: Program.cs

--- Program.cs
+++ Program.cs
@@ -15,40 +15,56 @@ public static class Formatter
         using var genPackage = new ExcelPackage(new FileInfo(generatedExcelFilePath));
         using var toFormatPackage = new ExcelPackage(new FileInfo(toFormatExcelFilePath));
 
-        // Basic Directions
-        BasicDirectionsClass.DefaultData(genPackage.Workbook.Worksheets[0], toFormatPackage.Workbook.Worksheets[0]);
+        // var cts = new CancellationTokenSource();
+        // Task spinnerTask = HelperFunctions.StartSpinner(cts.Token);
 
-        var directions = BasicDirectionsClass.FindAllDirections(genPackage);
-        for (var direction = 1; direction <= directions; direction++)
+        try
         {
-            BasicDirectionsClass.BasicDirections(direction, genPackage, toFormatPackage);
+            // Basic Directions
+            BasicDirectionsClass.DefaultData(genPackage.Workbook.Worksheets[0], toFormatPackage.Workbook.Worksheets[0]);
+
+            var directions = BasicDirectionsClass.FindAllDirections(genPackage);
+            for (var direction = 1; direction <= directions; direction++)
+            {
+                BasicDirectionsClass.BasicDirections(direction, genPackage, toFormatPackage);
+            }
+
+            // Total Intensity Rundown
+            var genWs =  TirClass.FindCorrectWorksheet(genPackage);
+            var toFormatWs =  TirClass.Prepare(toFormatPackage);
+
+            TirClass.FormatMeasuredTime(genWs, toFormatWs);
+            var totalVehicleCategoryCount = TirClass.FormatVehicleCategories(genWs, toFormatWs);
+
+            var tirPrimaryDataMapping = TirClass.ReadPrimaryData(genPackage, toFormatPackage);
+            TirClass.WritePrimaryData(tirPrimaryDataMapping, toFormatWs);
+
+            var addedUpRowData = TirClass.CalculateAddedUpRowData(toFormatWs);
+            var addedUpColumnData = TirClass.CalculateAddedUpColumnData(toFormatWs);
+
+            TirClass.CheckForMatchingResults(toFormatWs, addedUpRowData, addedUpColumnData);
+            TirClass.Styling(toFormatWs);
+
+            // Total Volume Class Breakdown
+            toFormatWs = TvcbClass.Prepare(toFormatPackage);
+            genWs = TvcbClass.FindCorrectWorksheet(genPackage);
+            TvcbClass.FormatMeasuredTime(genWs, toFormatWs);
+            TvcbClass.SecondaryNavigation(toFormatWs, genWs);
+            var (listsOfAllData, listOfDirectionColumns, allDirectionsList) = TvcbClass.PrimaryDataReading(genWs, toFormatWs);
+            TvcbClass.PrimaryDataWriting(genWs, toFormatWs, listsOfAllData, listOfDirectionColumns, allDirectionsList);
+            TvcbClass.Style(toFormatWs);
+
+            // AM, Midday, PM Peaks
+            DayPeaksClass.GenerateWorksheet(toFormatPackage, genPackage);
+
+            // Program End
+            toFormatPackage.Save();
+        }
+        finally
+        {
+            // cts.Cancel();
         }
 
-        // Total Intensity Rundown
-        var genWs =  TirClass.FindCorrectWorksheet(genPackage);
-        var toFormatWs =  TirClass.Prepare(toFormatPackage);
-
-        TirClass.FormatMeasuredTime(genWs, toFormatWs);
-        var totalVehicleCategoryCount = TirClass.FormatVehicleCategories(genWs, toFormatWs);
-
-        var tirPrimaryDataMapping = TirClass.ReadPrimaryData(genPackage, toFormatPackage);
-        TirClass.WritePrimaryData(tirPrimaryDataMapping, toFormatWs);
-
-        var addedUpRowData = TirClass.CalculateAddedUpRowData(toFormatWs);
-        var addedUpColumnData = TirClass.CalculateAddedUpColumnData(toFormatWs);
-
-        TirClass.CheckForMatchingResults(toFormatWs, addedUpRowData, addedUpColumnData);
-        TirClass.Styling(toFormatWs);
-
-        // Total Volume Class Breakdown
-        toFormatWs = TvcbClass.Prepare(toFormatPackage);
-        genWs = TvcbClass.FindCorrectWorksheet(genPackage);
-        TvcbClass.FormatMeasuredTime(genWs, toFormatWs);
-        TvcbClass.Navigation(genWs, toFormatWs, directions);
-        TvcbClass.Style(toFormatWs);
-
-        // Program End
-        toFormatPackage.Save();
         Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] ----- Program Finished -----]");
     }
 }
